43375c149e21e559090994586a720bd3742d38c8
JohnDaise <jdaise@gmail.com>
2023-05-28 18:08:08 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which ninja turtle do you believe will win the bet? "
+                                                          "Enter a color: ")
+tim = Turtle
+
+
+screen.exitonclick()

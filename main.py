0babc454b34c3ab7f25b6760640c58322d10c1aa
JohnDaise <jdaise@gmail.com>
2023-05-28 18:08:42 -0400

    small fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which ninja turtle do you believe will win the bet? "
                                                           "Enter a color: ")
-tim = Turtle
+tim = Turtle()
 
 
 screen.exitonclick()

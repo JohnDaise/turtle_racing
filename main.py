3e403e1a16c42f5ab4f165ea441ab7ca6554fc82
JohnDaise <jdaise@gmail.com>
2023-05-28 18:19:31 -0400

    adjusted turtle positioning

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which ninja turtle do you believe will win the race? "
                                                           "Enter a color: ")
-colors = ["red", "orange", "purple", "blue", "green"]
+colors = ["red", "orange", "purple", "blue", "green", "black"]
 
-starting_point = -100
+y_starting_pos = -130
 
 for i in range(len(colors)):
     tim = Turtle("turtle")
     tim.color(colors[i])
     tim.penup()
-    tim.goto(x=-230, y=starting_point)
-    starting_point += 50
+    tim.goto(x=-230, y=y_starting_pos)
+    y_starting_pos += 50
 
 
 screen.exitonclick()
